bd461293045b3bf3162b292378adff42a5e8a186
Sam Earl <samueltremain@gmail.com>
2019-05-29 17:45:40 -0500

    Add some notes and make some minor code changes

--- src/server/server.js
+++ src/server/server.js
@@ -9,15 +9,18 @@ const app = express();
 app.use(express.static(path.join(__dirname, "client/build")));
 
 // An api endpoint that returns a short list of items
-app.get("/api/getList", (req,res) => {
+app.get("/api/getList", (req, res) => {
     var list = ["item1", "item2", "item3"];
     res.json(list);
     console.log("Sent list of items");
 });
 
-// Return the same index.html file for every request so the app can use BrowsserHistory in production
-// SHOULD THIS BE app.all() INSTEAD OF app.get() SO THAT index.html WILL BE RETURNED FOR EVERY
-// REQUEST REGARDLESS OF THE METHOD IN THE REQUEST?
+/**
+ * All other routes and API endpoints should go above this one.
+ * -------------------------------------------------------------
+ * Catch-all route that returns the same index.html file for every request so the app can use
+ * BrowserHistory in production.
+ */
 app.get("*", (req, res) => {
   if (env === "production") {
     res.sendFile(path.join(__dirname + "some/path"));
