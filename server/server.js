79930aecfd8ebf9a1206bf35f4bd6d9e453a012e
Sam Earl <samueltremain@gmail.com>
2019-06-04 08:26:12 -0500

    Make minor changes to file organization

--- server/server.js
+++ server/server.js
@@ -30,7 +30,7 @@ app.get("*", (req, res) => {
   }
 });
 
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 4000;
 app.listen(port);
 
-console.log("App is listening on port " + port);
+console.log(`App is listening on port ${port}`);
